3dc00c879edd29d229976ce9d458f35a8eb63eea
Subhamdas20 <subham21.sd@gmail.com>
2021-10-09 00:24:23 +0530

    modified the method name.

--- src/test/java/com/bz/testregex/TestUserRegistration.java
+++ src/test/java/com/bz/testregex/TestUserRegistration.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class TestUserRegistration {
     @Test
-    public void givenAEmailPatternWhenMatchedShouldReturnTrue(){
+    public void givenAMobileNumberPatternWhenMatchedShouldReturnTrue(){
         UserRegistration user = new UserRegistration();
         boolean number = user.numberCheck("91 7003321213");
         Assert.assertTrue(number);
