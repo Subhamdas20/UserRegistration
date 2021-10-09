a43075d10cd1a33f5f613f4c2e4c079e0753fd17
Subhamdas20 <subham21.sd@gmail.com>
2021-10-09 13:46:18 +0530

    Regex pattern for atleast 1 uppercase in the password

--- src/main/java/com/bz/regex/UserRegistration.java
+++ src/main/java/com/bz/regex/UserRegistration.java
@@ -1,11 +1,10 @@
 package com.bz.regex;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.regex.*;
 
 public class UserRegistration {
 
     public boolean checkPassword(String password){
-        boolean check = Pattern.matches("[a-z]{8}", password);
+        boolean check = Pattern.matches("^(?=.*[A-Z])[A-Za-z]{8,}$", password);
         return check;
     }
 }
