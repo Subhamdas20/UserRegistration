6090e501a6464bb0a6b255877eae2272de4581bd
Subhamdas20 <subham21.sd@gmail.com>
2021-10-09 21:28:40 +0530

    checked sample emails

--- src/main/java/com/bz/regex/UserRegistration.java
+++ src/main/java/com/bz/regex/UserRegistration.java
@@ -3,8 +3,8 @@ import java.util.regex.*;
 
 public class UserRegistration {
 
-    public boolean checkPassword(String password){
-        boolean check = Pattern.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*?&])[A-Za-z0-9]{8,}[@$!%*?&]{1}$", password);
+    public boolean emailCheck(String name){
+        boolean check = Pattern.matches("([a-zA-Z0-9.-_]{3,})[a-zA-Z0-9.-_]*[@][a-zA-Z0-9.-_]*[a-zA-Z0-9.-_]{3,}[a-zA-Z0-9.-_]*",name);
         return check;
     }
 }
--- src/test/java/com/bz/testregex/TestUserRegistration.java
+++ src/test/java/com/bz/testregex/TestUserRegistration.java
@@ -6,9 +6,46 @@ import org.junit.Test;
 
 public class TestUserRegistration {
     @Test
-    public void givenAPasswordPatternWhenMatchedShouldReturnTrue(){
+    public void givenAEmailPatternWhenMatchedShouldReturnTrue(){
         UserRegistration user = new UserRegistration();
-        boolean password = user.checkPassword("Xxxxxxx9@");
-        Assert.assertTrue(password);
+        boolean email = user.emailCheck("abc.zxy@yahoo.com.in");
+        Assert.assertTrue(email);
     }
+    @Test
+    public void givenAEmailPatternWhenMatchedShouldReturnTrue2(){
+        UserRegistration user = new UserRegistration();
+        boolean email = user.emailCheck("abc-100@yahoo.com");
+        Assert.assertTrue(email);
+    }
+    @Test
+    public void givenAEmailPatternWhenMatchedShouldReturnTrue3(){
+        UserRegistration user = new UserRegistration();
+        boolean email = user.emailCheck("abc.100@yahoo.com");
+        Assert.assertTrue(email);
+    }
+    @Test
+    public void givenAEmailPatternWhenMatchedShouldReturnTrue4(){
+        UserRegistration user = new UserRegistration();
+        boolean email = user.emailCheck("abc111@abc.com");
+        Assert.assertTrue(email);
+    }
+    @Test
+    public void givenAEmailPatternWhenMatchedShouldReturnTrue5(){
+        UserRegistration user = new UserRegistration();
+        boolean email = user.emailCheck("abc-100@yahoo.net");
+        Assert.assertTrue(email);
+    }
+    @Test
+    public void givenAEmailPatternWhenMatchedShouldReturnTrue6(){
+        UserRegistration user = new UserRegistration();
+        boolean email = user.emailCheck("abc.100@abc.com.au");
+        Assert.assertTrue(email);
+    }
+    @Test
+    public void givenAEmailPatternWhenMatchedShouldReturnTrue7(){
+        UserRegistration user = new UserRegistration();
+        boolean email = user.emailCheck("abc@gmail.com.com");
+        Assert.assertTrue(email);
+    }
+
 }
