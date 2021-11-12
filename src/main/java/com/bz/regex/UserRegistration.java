31472c9da6c8d51bd06885ab4f64b7f37e6d1ed0
Subhamdas20 <subham21.sd@gmail.com>
2021-11-12 23:24:17 +0530

    Added : uc11 parameterised junit

--- src/main/java/com/bz/regex/UserRegistration.java
+++ src/main/java/com/bz/regex/UserRegistration.java
@@ -1,10 +1,11 @@
 package com.bz.regex;
+
 import java.util.regex.*;
 
 public class UserRegistration {
 
-    public boolean emailCheck(String name){
-        boolean check = Pattern.matches("([a-zA-Z0-9.-_]{3,})[a-zA-Z0-9.-_]*[@][a-zA-Z0-9.-_]*[a-zA-Z0-9.-_]{3,}[a-zA-Z0-9.-_]*",name);
+    public boolean emailCheck(String name) {
+        boolean check = Pattern.matches("([a-zA-Z0-9-_]{3,})[a-zA-Z0-9.-_]*[@][a-zA-Z0-9]*[a-zA-Z0-9.-_]{3,}[a-zA-Z0-9.-_]*", name);
         return check;
     }
 }
--- src/test/java/com/bz/testregex/TestUserRegistration.java
+++ src/test/java/com/bz/testregex/TestUserRegistration.java
@@ -2,50 +2,43 @@ package com.bz.testregex;
 
 import com.bz.regex.UserRegistration;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class TestUserRegistration {
-    @Test
-    public void givenAEmailPatternWhenMatchedShouldReturnTrue(){
-        UserRegistration user = new UserRegistration();
-        boolean email = user.emailCheck("abc.zxy@yahoo.com.in");
-        Assert.assertTrue(email);
+
+    private boolean expected_Result;
+    private String email;
+    private UserRegistration user;
+
+    public TestUserRegistration(boolean expected_Result, String email) {
+        this.expected_Result = expected_Result;
+        this.email = email;
     }
-    @Test
-    public void givenAEmailPatternWhenMatchedShouldReturnTrue2(){
-        UserRegistration user = new UserRegistration();
-        boolean email = user.emailCheck("abc-100@yahoo.com");
-        Assert.assertTrue(email);
+
+    @Before
+    public void initialize() {
+        user = new UserRegistration();
     }
-    @Test
-    public void givenAEmailPatternWhenMatchedShouldReturnTrue3(){
-        UserRegistration user = new UserRegistration();
-        boolean email = user.emailCheck("abc.100@yahoo.com");
-        Assert.assertTrue(email);
+
+    @Parameterized.Parameters
+    public static Collection input() {
+        return Arrays.asList(new Object[][]{{true, "abc.zxy@yahoo.com.in"}, {true, "abc.100@yahoo.com"},
+                {true, "abc111@abc.com"}, {true, "abc-100@yahoo.net"}, {true, "abc.100@abc.com.au"}, {true, "abc@gmail.com.com"},
+                {true, "abc@.com.my"}, {false, "abc()*@gmail.com"}, {false, ".abc@abc.com"},{false,".abc@abc.com"}});
     }
+
     @Test
-    public void givenAEmailPatternWhenMatchedShouldReturnTrue4(){
+    public void givenAEmailPatternWhenMatchedShouldReturnTrue() {
         UserRegistration user = new UserRegistration();
-        boolean email = user.emailCheck("abc111@abc.com");
-        Assert.assertTrue(email);
+        Assert.assertEquals(expected_Result, user.emailCheck(email));
     }
-    @Test
-    public void givenAEmailPatternWhenMatchedShouldReturnTrue5(){
-        UserRegistration user = new UserRegistration();
-        boolean email = user.emailCheck("abc-100@yahoo.net");
-        Assert.assertTrue(email);
-    }
-    @Test
-    public void givenAEmailPatternWhenMatchedShouldReturnTrue6(){
-        UserRegistration user = new UserRegistration();
-        boolean email = user.emailCheck("abc.100@abc.com.au");
-        Assert.assertTrue(email);
-    }
-    @Test
-    public void givenAEmailPatternWhenMatchedShouldReturnTrue7(){
-        UserRegistration user = new UserRegistration();
-        boolean email = user.emailCheck("abc@gmail.com.com");
-        Assert.assertTrue(email);
-    }
+
 
 }
